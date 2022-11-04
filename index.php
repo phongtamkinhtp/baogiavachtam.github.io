38164623a0a5fa6ba4d7cca3690376f5bda93f73
phongtamkinhtp <117432462+phongtamkinhtp@users.noreply.github.com>
2022-11-04 14:14:38 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="container1">
+        
+        <div id="slide"  class="slider">
+            <div class="item" data-index="6" style="background-image: url(1.jpg);">
+                <div class="content">
+                    <div class="name">SAINT<br> ANTONIEN</div>
+                    <div class="des">We take pride to provide Design and build for residential, from simple old houses to beautifully design dwellings, offices, commercial and industrial projects. </div>
+                     <a class="button" href="https://kinhopbepthanhphat.com/">See more</a>
+                </div>
+            </div>
+            <div class="item" data-index="1" style="background-image: url(2.jpg);">
+                <div class="content">
+                    <div class="name">NAGAGO<br> PREFECTURE</div>
+                    <div class="des">We take pride to provide Design and build for residential, from simple old houses to beautifully design dwellings, offices, commercial and industrial projects. </div>
+                    
+                    <a class="button" href="https://kinhtrangtrithanhphat.com/">See more</a>
+                </div>
+            </div>
+            <div class="item" data-index="2" style="background-image: url(3.jpg);">
+                <div class="content">
+                    <div class="name">MARRAKECH<br> MERZOUGA</div>
+                    <div class="des">We take pride to provide Design and build for residential, from simple old houses to beautifully design dwellings, offices, commercial and industrial projects. </div>
+                    <a class="button" href="https://kinhopbepthanhphat.com/bao-gia-tranh-kinh/">See more</a>
+                </div>
+            </div>
+            <div class="item" data-index="3"  style="background-image: url(4.jpg);">
+                <div class="content">
+                    <div class="name">YOSEMITE<br>NATIONAL PARK</div>
+                    <div class="des">We take pride to provide Design and build for residential, from simple old houses to beautifully design dwellings, offices, commercial and industrial projects. </div>
+                     <a class="button" href="https://kinhtrangtrithanhphat.com/bao-gia-kinh-trang-tri/">See more</a>
+                </div>
+            </div>
+            <div class="item" data-index="4"  style="background-image: url(5.jpg);">
+                <div class="content">
+                    <div class="name">LOS LANCES<br>BEACH</div>
+                    <div class="des">We take pride to provide Design and build for residential, from simple old houses to beautifully design dwellings, offices, commercial and industrial projects. </div>
+                    <a class="button" href="https://phongtamkinhthanhphat.com/">See more</a>
+                </div>
+            </div>
+            <div class="item" data-index="5"  style="background-image: url(6.jpg);">
+                <div class="content">
+                    <div class="name">NAGAGO<br> PREFECTURE</div>
+                    <div class="des">We take pride to provide Design and build for residential, from simple old houses to beautifully design dwellings, offices, commercial and industrial projects. </div>
+                     <a class="button" href="https://phuongnamglass.com/">See more</a>
+                </div>
+            </div>
+        </div>
+        <div class="buttons">
+            <button id="prev"><i class="fa-solid fa-angle-left"></i></button>
+            <button id="next"><i class="fa-solid fa-angle-right"></i></button>
+        </div>
+        <div class="nav-slider-dev">
+           
+            <div class="process-slider">
+                <div class="percent-process" style="width: 40%;"></div>
+                <div class="line-processs"></div>
+            </div>
+             <div class="current-slider">02</div>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
